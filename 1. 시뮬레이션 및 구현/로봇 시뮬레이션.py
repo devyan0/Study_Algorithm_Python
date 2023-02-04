7e60f9a028c03349828af29e2f22b98b24381b51
Seunghyun Yang <haeundae96@gmail.com>
2023-02-04 15:32:57 +0900

    comment added

--- 1. 시뮬레이션 및 구현/로봇 시뮬레이션.py
+++ 1. 시뮬레이션 및 구현/로봇 시뮬레이션.py
@@ -9,68 +9,23 @@
 """
 
 import sys
-
-sys.setrecursionlimit(int(1e9))
 input = sys.stdin.readline
 
-# row B col A
-A, B = map(int, input().split())
+N, K = map(int, input().split())
+origin, res = list(range(1, N + 1)), []
+i = -1
 
-# N robots, M operations
-N, M = map(int, input().split())
+# iterate until result list if filled
+while len(res) < N:
+    # effective move bypassing the duplicate
+    move = 0
+    while move < K:
+        # check if movable
+        if origin[(i + 1) % N]: move += 1
+        i = (i + 1) % N
 
-d = {}
-X, Y, DIR = 0, 1, 2
+    # next number found
+    res.append(origin[i])
+    origin[i] = 0
 
-NESW = ['N', 'E', 'S', 'W']
-MOVE = {'N': (0, 1), 'W': (-1, 0), 'S': (0, -1), 'E': (1, 0)}
-
-def R(cur, cnt=1):
-    idx = NESW.index(cur)
-    return NESW[(idx+cnt) % 4]
-
-def L(cur, cnt=1):
-    idx = NESW.index(cur)
-    return NESW[(idx-cnt) % 4]
-
-def forward(num, repeat, x, y, dir):
-    dx, dy = MOVE[dir]
-    for i in range(1, repeat+1):
-        nx, ny = x+dx*i, y+dy*i
-        if not (0<nx<=A and 0<ny<=B):
-            print(f'Robot {num} crashes into the wall')
-            return False
-        for other in d:
-            if other == num: continue
-            ox, oy, _ = d[other]
-            if (nx, ny) == (ox, oy):
-                print(f'Robot {num} crashes into robot {other}')
-                return False
-        d[num] = [nx, ny, dir]
-    return True
-
-def operate():
-    res = True
-    for _ in range(M):
-        num, op, repeat = input().split()
-        if not res: continue
-
-        num, repeat = int(num), int(repeat)
-        if op == 'R': d[num][DIR] = R(d[num][DIR], repeat)
-        if op == 'L': d[num][DIR] = L(d[num][DIR], repeat)
-        if op == 'F':
-            if not forward(num, repeat, *d[num]):
-                res = False
-
-    return res
-
-for num in range(1, N+1):
-    x, y, dir = input().split()
-    d[num] = [int(x), int(y), dir]
-
-
-
-if operate():
-    print('OK')
-
-
+print('<' + ', '.join([str(n) for n in res]) + '>')
--- 1. 시뮬레이션 및 구현/요세푸스 문제 0.py
+++ 1. 시뮬레이션 및 구현/요세푸스 문제 0.py
@@ -9,8 +9,6 @@
 """
 
 import sys
-
-sys.setrecursionlimit(int(1e9))
 input = sys.stdin.readline
 
 # row B col A
@@ -19,57 +17,71 @@ A, B = map(int, input().split())
 # N robots, M operations
 N, M = map(int, input().split())
 
+# d: {num: [x, y, heading_dir], ...}
 d = {}
 X, Y, DIR = 0, 1, 2
 
 NESW = ['N', 'E', 'S', 'W']
 MOVE = {'N': (0, 1), 'W': (-1, 0), 'S': (0, -1), 'E': (1, 0)}
 
-def R(cur, cnt=1):
-    idx = NESW.index(cur)
-    return NESW[(idx+cnt) % 4]
+# return the new direction of the robot
+def rotate(cur, cnt, op):
+    next_idx = NESW.index(cur) + (cnt if op == 'R' else -cnt)
+    return NESW[next_idx % 4]
 
-def L(cur, cnt=1):
-    idx = NESW.index(cur)
-    return NESW[(idx-cnt) % 4]
-
-def forward(num, repeat, x, y, dir):
+# the robot #num moves forward repeat times
+def forward(num, repeat):
+    x, y, dir = d[num]
     dx, dy = MOVE[dir]
+
+    # move to the heading direction repeat times
     for i in range(1, repeat+1):
         nx, ny = x+dx*i, y+dy*i
-        if not (0<nx<=A and 0<ny<=B):
+
+        # case 1: out of bound
+        if not (0 < nx <= A and 0 < ny <= B):
             print(f'Robot {num} crashes into the wall')
             return False
+
+        # case 2: collision with another robot
         for other in d:
             if other == num: continue
             ox, oy, _ = d[other]
             if (nx, ny) == (ox, oy):
                 print(f'Robot {num} crashes into robot {other}')
                 return False
+
+        # if moveable, update location
         d[num] = [nx, ny, dir]
+
     return True
+
 
 def operate():
     res = True
     for _ in range(M):
         num, op, repeat = input().split()
-        if not res: continue
-
         num, repeat = int(num), int(repeat)
-        if op == 'R': d[num][DIR] = R(d[num][DIR], repeat)
-        if op == 'L': d[num][DIR] = L(d[num][DIR], repeat)
+
+        if not res:
+            continue    # failure already found
+
+        # operation 1: move forward
         if op == 'F':
-            if not forward(num, repeat, *d[num]):
-                res = False
+            res = forward(num, repeat)
+        # operation 2, 3: rotate left or right
+        else:
+            d[num][DIR] = rotate(d[num][DIR], repeat, op)
 
     return res
 
+
+# take input
 for num in range(1, N+1):
     x, y, dir = input().split()
     d[num] = [int(x), int(y), dir]
 
-
-
+# run the test case
 if operate():
     print('OK')
 
