4d422dc3cb680547982d3702c8ee55486b2afda8
양승현 <haeundae96@gmail.com>
2023-02-15 16:15:05 +0900

    solved

--- 0. 18반 알고리즘 문제/BOJ/BOJ_1374_강의실.py
+++ 0. 18반 알고리즘 문제/BOJ/BOJ_1374_강의실.py
@@ -3,19 +3,25 @@ import sys
 input = sys.stdin.readline
 
 N = int(input())
-info = [list(map(int, input().split()[1:][::-1])) for x in range(N)]
+# info = [list(map(int, input().split()[1:][::-1])) for x in range(N)]
+info = [list(map(int, input().split()[1:])) for x in range(N)]
 # [(end, start), ...]
-heapify(info)
+info.sort()
+print(info)
 res, q = 0, []
 
 for i in info:
+    print(f'{i} in')
     s, e = i
     if not q:
-        push(q, i)
+        push(q, e)
     else:
-        if q[0][0] <= s:    # 가장 이른 종료 시간
+        if q[0] <= s:    # 가장 이른 종료 시간
+            print(f'{q[0]} will pop')
             pop(q)
-        push(q, i)
+        push(q, e)
+    print(f'now: {q}')
+    print()
 
     res = max(res, len(q))
 
--- test.py
+++ test.py
@@ -1,19 +1,61 @@
 """
-문제 정보
-    https://www.acmicpc.net/problem/15732
-    * filter condition
+https://www.acmicpc.net/problem/1781
 
-작성
-    양승현
-    2023 02 09
+익숙하지 않은 유형
+정합성 판단이 잘 안 서서 정렬로 풀려고 했는데 모순
+"""
+from heapq import heappush as push, heappop as pop
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+info = sorted([list(map(int, input().split())) for _ in range(N)])
+
+res, save, t = 0, 1, info[-1][0]
+tasks = []
+while 0 < t:
+    while info and t <= info[-1][0]:
+        push(tasks, -info.pop()[1])
+
+    if 0 < save and tasks:
+        res += -pop(tasks)
+        save -= 1
+
+    t, save = t-1, save+1
+
+print(res)
+
 """
 
-N, K, D = map(int, input().split())
-info, l, r = [list(map(int, input().split())) for _ in range(K)], 1, N+1
-prev_cnt = lambda n: sum(filter(lambda k: 0 < k, [1 + (min(n, x[1])-x[0])//x[2] for x in info]))
-while l < r:
-    mid = (l+r) // 2
-    cnt = prev_cnt(mid)
-    if cnt >= D: r = mid
-    else: l = mid + 1
-print(l)
+3
+3 5
+3 4
+1 1
+=> 10
+
+
+9
+5 5
+4 6
+4 12
+3 8
+4 18
+2 10
+2 5
+1 7
+1 14
+
+
+
+와 같은 경우, 위와 같이 해결하면
+
+1일 14개, 2일 10개, 3일 8개, 4일 12개, 5일 5개를 받으나
+
+1일날 14개 (1일데드라인)
+2일날 10개 (2일데드라인)
+3일날 18개 (4일데드라인)
+4일날 12개 (4일데드라인)
+5일날 5개 (5일데드라인)
+
+이 최선의 경우입니다.
+"""
