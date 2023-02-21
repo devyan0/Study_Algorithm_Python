f05e7f03f0a1d202adce394ecf4eb1a591429a77
양승현 <haeundae96@gmail.com>
2023-02-21 11:17:01 +0900

    failed

---
+++ 프로그래머스/level4_2.py
@@ -0,0 +1,23 @@
+
+def solution(*arg):
+    ...
+
+
+
+
+args = [
+    [["SOOOL", "XXXXO", "OOOOO", "OXXXX", "OOOOE"]],
+    [["LOOXS", "OOOOX", "OOOOO", "OOOOO", "EOOOO"]]
+]
+
+sols = [
+    16,
+    -1
+]
+
+for a, s in zip(args, sols):
+    res = solution(*a)
+    if res != s:
+        print(f'result {res} differ from {s}')
+
+print('test finished')
