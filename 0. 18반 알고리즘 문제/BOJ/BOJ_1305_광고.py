df6561cdbfbb23a7651da752f8598cb63deae9a6
Seunghyun Yang <haeundae96@gmail.com>
2023-03-04 20:05:53 +0900

    .

---
+++ 0. 18반 알고리즘 문제/BOJ/BOJ_1305_광고.py
@@ -0,0 +1,22 @@
+import sys
+
+sys.stdin = open('input.txt', 'r')
+
+input = sys.stdin.readline
+
+def get_pi(p):
+    pi, j = [0] * len(p), 0
+
+    for i in range(1, len(p)):
+        while j > 0 and p[i] != p[j]:
+            j = pi[j-1]
+
+        if p[i] == p[j]:
+            j += 1
+            pi[i] = j
+    return pi
+
+N = int(input())
+L = input()
+pi = get_pi(L)
+print(N-pi[-1])
