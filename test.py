285e4624d564de85b6b3839ff6816afc5ee6c521
Seunghyun Yang <haeundae96@gmail.com>
2023-03-05 23:08:32 +0900

    .

--- test.py
+++ test.py
@@ -1,9 +1,31 @@
-prices = [7, 1, 5, 3, 6, 4]
-days = len(prices)
-profit = 0
+class Solution:
+    def findValidSplit(self, nums) -> int:
+        from math import gcd
+        from functools import reduce
 
-for buy in range(days):
-    for sell in range(buy+1, days):
-        profit = max(profit, prices[sell]-prices[buy])
+        for i, n in enumerate(nums):
+            d = 2
+            while d < n:
+                if n % d != 0:
+                    d += 1
+                    continue
+                while n % d == 0:
+                    n = n // d
+                n *= d
+                d += 1
 
-print(profit)
+            nums[i] = n
+
+        prod = reduce(lambda x, y: x * y, nums)
+        left = 1
+
+        for i, n in enumerate(nums):
+            left = left * n
+            prod = prod // n
+            if gcd(left, prod) == 1:
+                return i
+
+            if i == len(nums) - 2: return -1
+
+
+res = Solution().findValidSplit([4,7,15,8,3,5])
