03a37d39e4449048507c5092b7c1a4f16c3e7358
Seunghyun Yang <haeundae96@gmail.com>
2023-03-07 23:46:07 +0900

    solved

---
+++ 0. 18반 알고리즘 문제/BOJ/BOJ_16922_로마숫자만들기.py
@@ -0,0 +1,19 @@
+import sys
+sys.stdin = open('input.txt', 'r')
+
+from functools import lru_cache
+
+N = int(input())
+res = set()
+
+@lru_cache(maxsize=1_000_000)
+def bt(cnt, temp):
+    if cnt == N:
+        res.add(temp)
+        return
+
+    for add in [1, 5, 10, 50]:
+        bt(cnt+1, temp+add)
+
+bt(0, 0)
+print(len(res))
