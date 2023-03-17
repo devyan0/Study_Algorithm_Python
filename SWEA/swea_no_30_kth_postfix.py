9c3c838f26cddbf7c7382c5d9abd3adf33148ba5
Seunghyun Yang <haeundae96@gmail.com>
2023-03-17 21:58:41 +0900

    solved

--- SWEA/swea_no_30_kth_postfix.py
+++ SWEA/swea_no_30_kth_postfix.py
@@ -6,10 +6,11 @@ from heapq import heappop as pop, heappush as push
 T = int(input())
 for tc in range(1, T+1):
     K, s = int(input()), input()
+    if len(s) <= K: print(f'#{tc} none'); continue
     temp, q = '', []
 
     for i in range(len(s)-1, -1, -1):
-        temp += s[i]
+        temp = s[i] + temp
         push(q, temp)
 
     res = ''
