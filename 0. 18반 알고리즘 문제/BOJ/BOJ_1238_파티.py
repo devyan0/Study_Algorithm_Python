b5f427265ad5a5f222a906b24987fd26dacfb9ae
백건우 <haeundae96@gmail.com>
2023-03-29 09:46:09 +0900

    solved

---
+++ 0. 18반 알고리즘 문제/BOJ/BOJ_1238_파티.py
@@ -0,0 +1,44 @@
+import sys
+sys.stdin = open('input.txt', 'r')
+
+"""
+Idea: run Dijkstra twice with
+    1. original graph: for each node, from X -> go back home
+    2. reversed path: come to X <- from each node
+    3. sum up, get max
+"""
+
+from heapq import heappush as push, heappop as pop
+
+N, M, X = map(int, input().split())
+origin = [[float('inf') for _ in range(N+1)] for _ in range(N+1)]
+reverse = [[float('inf') for _ in range(N+1)] for _ in range(N+1)]
+
+for _ in range(M):
+    s, e, c = map(int, input().split())
+    origin[s][e] = reverse[e][s] = c
+
+
+def run_dijkstra(graph, start=X):
+    q = [(0, start)]
+    visited = set()
+    dist = [float('inf') for _ in range(N+1)]
+
+    while q:
+        dist_cur, cur = pop(q)
+        if cur in visited: continue
+        visited.add(cur)
+        dist[cur] = dist_cur
+
+        for adj, cost in enumerate(graph[cur]):
+            if cost == float('inf'): continue
+            push(q, (dist_cur+cost, adj))
+
+    return dist
+
+
+distance = [in_ + out_ for in_, out_ in zip(run_dijkstra(origin), run_dijkstra(reverse))]
+print(max(distance[1:]))
+
+
+
